48717478ff5ad5aef2e30acb13ed1f5e60e75539
Shubham <shubham@Shubhams-MacBook-Pro.local>
2026-01-12 00:24:47 +0530

    feat(matching): Implement Ashtakoot Guna Milan and Mangal Dosha check

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,8 @@ export * from './core/ayanamsa';
 export * from './core/panchangam';
 export * from './core/muhurta/types';
 export { Observer } from 'astronomy-engine';
+
+export * from './kundli/index';
+export * from './kundli/types';
+export * from './matching/index';
+export * from './matching/types';
